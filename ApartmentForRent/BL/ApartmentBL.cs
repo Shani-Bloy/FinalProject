f4bc4ca9e853167b1034a438ef861f725afa8fea
Shani-Bloy <sh6720398@gmail.com>
2021-08-22 21:43:45 +0300

    delete the function

--- ApartmentForRent/BL/ApartmentBL.cs
+++ ApartmentForRent/BL/ApartmentBL.cs
@@ -49,9 +49,5 @@ namespace BL
         {
             new Dal.ApartmentDAL().PostNewApartment(Converters.ApartmentConverter.GetApartmentFromDTO(apartment));
         }
-        public void printAll()
-        {
-            Console.WriteLine("hello every one!");
-        }
     }
 }
